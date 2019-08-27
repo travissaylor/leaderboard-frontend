d5182afe58b3980b84abc2b6a607eb36a92e8450
Adam Geiger <champtoday@me.com>
2019-08-27 18:37:04 -0400

    add post numbers and author name on authorpost table.

--- pages/AuthorPosts.js
+++ pages/AuthorPosts.js
@@ -2,8 +2,9 @@ import Layout from '../components/Layout';
 import { withRouter } from 'next/router';
 import fetch from 'isomorphic-unfetch';
 
-const AuthorPost = (props) => (
-        <Layout title="Author's Posts">
+const AuthorPost = withRouter( props => {
+    return(
+        <Layout title="Author's Posts" tableTitle={props.router.query.authorName}>
             <thead>
                 <tr>
                     <th width="50">RANK</th>
@@ -11,33 +12,33 @@ const AuthorPost = (props) => (
                     <th width="150"># UNIQUES</th>
                 </tr>
             </thead>
-            <Content posts={props.posts} />
+            <Content posts={props.posts} authorId={props.router.query.authorId}/>
         </Layout>
-);
-
-const Content = withRouter( props  => {
-    return(
-        <tbody>
-            {props.posts.map(post => {
-                if(post.userId == props.router.query.authorId) {
-                    return (
-                        <tr key={post.id}>
-                            <td className="ranking-cell">
-                            <div className="ranking">{post.id}</div>
-                            </td>
-                            <td className="post-cell">
-                            <div className="name">{post.title}</div>
-                            </td>
-                            <td className="unique-cell">
-                            <div className="unique-count">###</div>
-                            </td>
-                        </tr>
-                    );
-                }
-            })}
-        </tbody>
-    )
+    );
 });
+
+const Content = ({posts, authorId}) =>
+(
+    <tbody>
+        {posts.map(post => {
+            if(post.userId == authorId) {
+                return (
+                    <tr key={post.id}>
+                        <td className="ranking-cell">
+                        <div className="ranking">{post.id}</div>
+                        </td>
+                        <td className="post-cell">
+                        <div className="name">{post.title}</div>
+                        </td>
+                        <td className="unique-cell">
+                        <div className="unique-count">###</div>
+                        </td>
+                    </tr>
+                );
+            }
+        })}
+    </tbody>
+);
 
 AuthorPost.getInitialProps = async function() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
